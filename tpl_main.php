fcd9bc016e2575d6ff7dffc5869ffb4fd6c5b6cb
Vladimir Samojlov <v-moi@ya.ru>
2018-11-03 00:49:58 +0200

    Edit gengorka name

--- tpl_main.php
+++ tpl_main.php
@@ -88,11 +88,11 @@ Template Name: Главная страница
       <!-- Отдых в Генгорке -->
       <div class="lead text-uppercase text-weight-bold pt-3 pb-3">
         <span>Отдых в Генгорке 2018</span>
-        <a href="<?php echo get_post_type_archive_link( 'gengorkahotels' ); ?>" class="float-right btn btn-outline-info d-none d-md-block my-2 my-sm-0 mr-md-0 mr-sm-2">Все предложения</a>
+        <a href="<?php echo get_post_type_archive_link( 'gengorka' ); ?>" class="float-right btn btn-outline-info d-none d-md-block my-2 my-sm-0 mr-md-0 mr-sm-2">Все предложения</a>
       </div>
       <div class="row">
         <?php 
-          $custom_query = new WP_Query( array( 'post_type' => 'gengorkahotels','posts_per_page'=>'8', 'orderby'   => 'rand' ) );
+          $custom_query = new WP_Query( array( 'post_type' => 'gengorka','posts_per_page'=>'8', 'orderby'   => 'rand' ) );
           if ($custom_query->have_posts()) : while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
           <div class="col-md-3 mb-5">
             <a href="<?php the_permalink(); ?>">
@@ -114,7 +114,7 @@ Template Name: Главная страница
           </div>
         <?php endwhile; endif; ?>
         <div class="col-sm-12 d-md-none d-sm-block">
-          <a href="<?php echo get_post_type_archive_link( 'gengorkahotels' ); ?>" class="float-md-right btn btn-outline-info my-2 my-sm-0 mr-sm-2">Все предложения</a>
+          <a href="<?php echo get_post_type_archive_link( 'gengorka' ); ?>" class="float-md-right btn btn-outline-info my-2 my-sm-0 mr-sm-2">Все предложения</a>
         </div>
       </div>
       <!-- ПРИАЗОВКА -->
