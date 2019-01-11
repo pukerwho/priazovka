39c555e1a0b9021cfcb18b0dc0b4761006a479fe
Vladimir Samojlov <v-moi@ya.ru>
2019-01-11 16:25:42 +0200

    edit link to photoalbum

--- blocks/photoalbum/photoalbum-block.php
+++ blocks/photoalbum/photoalbum-block.php
@@ -5,6 +5,6 @@
   <div class="text-side bg-success float-left d-flex justify-content-center align-items-center text-white flex-column">
     <div class="display-4 text-uppercase">Фотоальбом</div>
     <div class="lead text-center py-3 px-3">Фотографии Арабатской стрелки 2019</div>
-    <div><a href="/photoalbums" class="display-4 text-white border-bottom">Смотреть</a></div>
+    <div><a href="/page-photoalbums" class="display-4 text-white border-bottom">Смотреть</a></div>
   </div>
 </div>
