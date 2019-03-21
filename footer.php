a039a1978703b1cf3999e2961f4789480f1b5c8d
Vladimir Samojlov <v-moi@ya.ru>
2019-03-22 00:06:15 +0200

    add link to vidpochivai

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
         <div class="row mb-3">
           <div class="col-md-4">
             <a href="<?php echo home_url(); ?>">Отдых в Геническе и на Арабатской стрелке</a>
+            <br>
+            <a href="https://vidpochivai.com.ua">Летний отдых в Украине | Отдых на море 2019</a>
           </div>
         </div>
       </div>
